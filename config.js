caa7dde255c1cffacb0cf8cca05987bb0dc9ea2c
Tanmay <kumartanmay109@gmail.com>
2026-05-17 17:25:18 +0530

    Deployment: Add config infrastructure for GitHub Pages validation

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js - Safe for GitHub Pages deployment
+export const firebaseConfig = {
+    apiKey: "AIzaSyDzp7JqZSjSpr_4rb_FNvYGTPE8cnLYnvM",
+    authDomain: "journal-94c51.firebaseapp.com",
+    projectId: "journal-94c51",
+    storageBucket: "journal-94c51.firebasestorage.app",
+    messagingSenderId: "213844260707",
+    appId: "1:213844260707:web:ab5708d2cf72008e198556",
+    databaseURL: "https://journal-94c51-default-rtdb.europe-west1.firebasedatabase.app"
+};
